dfb00af3b6474f815f07cc90d20bb6d3e12c1313
Charles Walker <charles.walker@tn.gov>
2016-10-14 14:37:34 -0500

    very rough set of changes, just to be able to access from home

--- tnrlib-v999/ViewController.swift
+++ tnrlib-v999/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-	}
+	@IBOutlet weak var tfSrc: UITextField!
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
+	@IBOutlet weak var tfAcct: UITextField!
+
+	@IBOutlet weak var tfPwd: UITextField!
 
 
+
+
+override
+func
+viewDidLoad() {
+
+	super.viewDidLoad()
+		
+	let myObj = TnHttpQa()
+
+	myObj.exampleOne()
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}  //	end of class
 
